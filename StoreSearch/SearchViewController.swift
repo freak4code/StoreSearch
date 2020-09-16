47a045203bd08b265d787228428eacb8a834c745
SNNAFI <shahriarnasim.nafi@gmail.com>
2020-09-16 06:39:59 +0600

    url session added

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -53,28 +53,43 @@ extension SearchViewController: UISearchBarDelegate{
             tableView.reloadData()
             let url = iTuneURL(search: searchBar.text!)
             print(url)
-            
-            let queue = DispatchQueue.global()
-            queue.async {
-                
-                if let jsonData = self.performStoreRequest(with: url){
-                    self.searchResults = self.parse(from: jsonData)
-                    // 1//
-                    //                searchResults.sort { $0.name.localizedStandardCompare($1.name) == .orderedAscending
-                    //                }
-                    //2//
-                    //                searchResults.sort{ $0 < $1}
-                    
-                    //3//
-                    self.searchResults.sort(by: <)
-                    DispatchQueue.main.async {
-                        self.hasSearched = true
-                        self.isLoading = false
-                        self.tableView.reloadData()
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Failure! \(error.localizedDescription)")
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    if let data = data{
+                        self.searchResults = self.parse(from: data)
+                        self.searchResults.sort(by: < )
+                        print("On main thread \(Thread.current.isMainThread ? "Yes" : "No")")
+                        DispatchQueue.main.async {
+                            self.hasSearched = true
+                            self.isLoading = false
+                            self.tableView.reloadData()
+                            print("On main thread \(Thread.current.isMainThread ? "Yes" : "No")")
+                        }
+                        return
+                        
                     }
-                    return
+                    
+                } else {
+                    print("Failure! \(response!)")
                 }
+                
+                DispatchQueue.main.async {
+                    self.hasSearched = false
+                    self.isLoading = false
+                    self.tableView.reloadData()
+                    self.showNetworkError()
+                }
+                
+                
             }
+            
+            dataTask.resume()
+            
+            
+            
             
         }
         
@@ -157,15 +172,7 @@ extension SearchViewController{
         return url
     }
     
-    func performStoreRequest(with url: URL) -> Data?{
-        do{
-            return try Data(contentsOf: url)
-        }catch{
-            print("Download Error: \(error.localizedDescription)")
-            showNetworkError()
-            return nil
-        }
-    }
+    
     
     func parse(from data: Data) -> [SearchResult]{
         do{
